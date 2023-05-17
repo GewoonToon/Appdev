fafdd125db5b66d8dc02b12b72b43e9fc4b52ebd
ToonDubois <toon.dubois@gmail.com>
2023-05-17 17:47:12 +0200

    Navigation bottom bar added

--- app/src/main/java/com/example/appdev/MainActivity.java
+++ app/src/main/java/com/example/appdev/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.appdev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.appdev.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            }
+            if(item.getItemId() == R.id.shop){
+                replaceFragment(new ShopFragment());
+            }
+            if(item.getItemId() == R.id.cart){
+                replaceFragment(new CartFragment());
+            }
+            if(item.getItemId() == R.id.settings){
+                replaceFragment(new SettingsFragment());
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
